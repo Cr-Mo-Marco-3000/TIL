e29eb2cb90df5753ebd061714dfc69b50f23c240
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2022-06-08 21:02:34 +0900

    블로그로 옮기기 전 정리 커밋

--- 99_problem_solving/JUNGOL/Begginner_Coder/1303_1856_1304_2046_숫자사각형1_2_3_4/1303.py
+++
@@ -1,8 +0,0 @@
-N, M = map(int, input().strip().split())
-
-cnt = 1
-for i in range(N):
-    for j in range(M):
-        print(cnt, end=" ")
-        cnt += 1
-    print()
--- 99_problem_solving/JUNGOL/Begginner_Coder/1303_1856_1304_2046_숫자사각형1_2_3_4/1304.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-cnt = 1
-for i in range(1, N+1):
-    for j in range(N):
-        print((i + (j * N)), end=" ")
-    print()
